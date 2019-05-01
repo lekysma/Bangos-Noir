fb9a5dc2e2fa7d61906009aa9f831a99fcef0412
lekysma <jeanmartinkyssama@yahoo.com>
2019-05-01 10:11:37 +0000

    test

--- View Controller/ChapitresTableViewController.swift
+++ View Controller/ChapitresTableViewController.swift
@@ -54,26 +54,6 @@ class ChapitresTableViewController: UITableViewController {
     
    
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-  
-  
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
