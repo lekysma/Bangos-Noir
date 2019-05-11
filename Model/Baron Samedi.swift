2bc742789515ce876b3d7d6f6af2775f23cab088
lekysma <jeanmartinkyssama@yahoo.com>
2019-05-11 20:12:43 +0000

    ajoute control flow pour l'histoire, résous un bug, astuce trouvé pour case : 3 mais un peu pauvre. voir si pas possible de trouver meilleure solution

--- Model/Baron Samedi.swift
+++ Model/Baron Samedi.swift
@@ -13,32 +13,33 @@ let rejouerBaronSamedi = "Rejouer"
 
 // 1
 let vaChercherGare : String = "Je vais chercher Fifion a la gare"
-let choix1VaChercherGare : String = "je consulte la photo donnée"
-let choix2VaChercherGare : String = "je ne consulte pas la photo"
+let choix1VaChercherGare : String = "je consulte la photo donnée" // vers 3
+let choix2VaChercherGare : String = "je ne consulte pas la photo" // vers 2
 // 2
-let mauvaixChoix : String = "mauvaise personne, je le trouve finalement"
-let choix1MauvaisChoix : String = "Suite"
+let mauvaixChoix : String = "mauvaise personne, qu'est ce que je fais"
+let choix1MauvaisChoix : String = "je regarde la photo" // vers 3
+let choix2MauvaisChoix : String = "je rentre bredouille" // vers 3 avec une variation
 // PS : pas de choix 2
 //3
-let necessiteFaireSacrifice : String = "je conseille mon patron : necessaire de le faire ?"
-let choix1NecessiteFaireSacrifice : String = "vous avez raison patron"
-let choix2NecessiteFaireSacrifice : String = "vous avez tort patron"
+var necessiteFaireSacrifice : String = "je conseille mon patron : necessaire de le faire ?"
+let choix1NecessiteFaireSacrifice : String = "vous avez raison patron" // vers 4
+let choix2NecessiteFaireSacrifice : String = "vous avez tort patron" // vers 17
 //4
 let chercherPrisonnier : String = "je vais chercher prisonnier, comment le preparer pour transport vers cimetiere ?"
-let choix1ChercherPrisonnier : String = "je le frappe"
-let choix2ChercherPrisonnier : String = "je le saoule"
+let choix1ChercherPrisonnier : String = "je le frappe" // vers 16
+let choix2ChercherPrisonnier : String = "je le saoule" // vers 5
 //5
-let prisonnierVomit : String = "le prisonnier vomit sur moi, on arrive cimmetiere, Fifion conseille offrande"
-let choix1PrisonnierVomit : String = "on n'a ni le temps ni le materiel"
-let choix2PrisonnierVomit : String = "peut-être qu'on devrait ecouter l'expert"
+let optionBoisson : String = "le prisonnier vomit sur moi, on arrive cimmetiere, Fifion conseille offrande"
+let choix1OptionBoisson : String = "on n'a ni le temps ni le materiel" // vers 6
+let choix2OptionBoisson : String = "peut-être qu'on devrait ecouter l'expert" // vers 7
 //6
 let baronApparait : String = "le patron sacrifie prisonnier, le baron apparait"
-let choix1BaronApparait : String = "on resiste"
-let choix2BaronApparait : String = "Fuyons"
+let choix1BaronApparait : String = "on resiste" // vers 9
+let choix2BaronApparait : String = "Fuyons" // vers 7
 //7
 let FuiteCimetiere : String = "nous fuyons, retour a la residence, decision"
-let choix1FuiteCimetiere : String = "je monte la garde"
-let choix2FuiteCimetiere : String = "je m'enfuis"
+let choix1FuiteCimetiere : String = "je monte la garde" // vers 8
+let choix2FuiteCimetiere : String = "je m'enfuis" // vers 10
 //8
 let finBaronSamedi1 : String = "j'affronte baron, meurs. news : crime horrible chez Makandal"
 // on rejouer, pas de choix 2
@@ -50,21 +51,27 @@ let finBaronSamedi3 : String = "je suis arreté par la police avec corps prisonn
 // on rejoue, pas de choix 2
 // 11
 let sacrificeRAS : String = "sacrifice RAS, on laisse le corps sur place ou on le rapporte ?"
-let choix1SacrificeRAS : String = "on laisse le corps au cimetiere"
-let choix2SacrificeRAS : String = "on emporte le corps avec nous"
+let choix1SacrificeRAS : String = "on laisse le corps au cimetiere" // vers 12
+let choix2SacrificeRAS : String = "on emporte le corps avec nous" // vers 13
 // 12
-let finBaronSamedi4 : String = "le baron apparait, mentiionne manque de respect, on meurt tous les deux"
+let finBaronSamedi4 : String = "le baron apparait, mentionne manque de respect, on meurt tous les deux"
 // on rejoue, pas de choix 2
 // 13
 let decisionSeDebarasserDuCorps : String = "on rapporte le corps dans voiture; comment se debarasser du corps ?"
-let choix1DecisionSeDebarasserDuCorps : String = "vers la mer"
-let choix2DecisionSeDebarasserDuCorps : String = "on l'enterre chez le patron"
+let choix1DecisionSeDebarasserDuCorps : String = "vers la mer" // vers 15
+let choix2DecisionSeDebarasserDuCorps : String = "on l'enterre chez le patron" // 14
 // 14
 let finBaronSamedi5 : String = "RAS, je finis par demissionner; news : decouverte macabre chez Makandal, je suis recherché"
 // on rejoue, pas de choix 2
 // 15
 let finBaronSamedi6 : String = "controle de police, le policier decouvre le corps. on se dechire au proces"
 // on rejoue, pas de choix 2
+//16
+let optionViolence : String = "prisonnier ensanglanté, on arrive au cimetiere, Fifion conseille offrande"
+let choix1OptionViolence : String = "on n'a ni le temps, ni le matos" // vers 6
+let choix2OptionViolence : String = "peut-etre qu'on devrait ecouter l'expert" // vers 11
+// 17
+let finBaronSamedi7 : String = "je demissionne, news : makandal a gagné elections, il meurt par la suite"
 
 
 
--- View Controller/Chapitre4ViewController.swift
+++ View Controller/Chapitre4ViewController.swift
@@ -50,6 +50,156 @@ class Chapitre4ViewController: UIViewController {
     }
     
     @IBAction func controlFlowHistoire4(_ sender: UIButton) {
+        
+        switch compteurHistoire4 {
+            // 1
+        case 1:
+            if sender.tag == 1 {
+                contenuHistoire4.text = necessiteFaireSacrifice
+                choix1.setTitle(choix1NecessiteFaireSacrifice, for: .normal)
+                choix2.setTitle(choix2NecessiteFaireSacrifice, for: .normal)
+                compteurHistoire4 = 3
+            } else {
+                contenuHistoire4.text = mauvaixChoix
+                choix1.setTitle(choix1MauvaisChoix, for: .normal)
+                choix2.setTitle(choix2MauvaisChoix, for: .normal)
+                compteurHistoire4 = 2
+                
+            }
+            // 2
+        case 2 :
+            if sender.tag == 1 {
+               contenuHistoire4.text = necessiteFaireSacrifice
+                choix1.setTitle(choix1NecessiteFaireSacrifice, for: .normal)
+                choix2.setTitle(choix2NecessiteFaireSacrifice, for: .normal)
+                compteurHistoire4 = 3
+            } else {
+                contenuHistoire4.text = """
+A votre grande surprise, la personne que vous cherchiez se trouve devant votre patron
+\(necessiteFaireSacrifice)
+"""
+                choix1.setTitle(choix1NecessiteFaireSacrifice, for: .normal)
+                choix2.setTitle(choix2NecessiteFaireSacrifice, for: .normal)
+                compteurHistoire4 = 3
+            }
+           
+            
+            // 3
+        case 3:
+            if sender.tag == 1 {
+                contenuHistoire4.text = chercherPrisonnier
+                choix1.setTitle(choix1ChercherPrisonnier, for: .normal)
+                choix2.setTitle(choix2ChercherPrisonnier, for: .normal)
+                compteurHistoire4 = 4
+            } else {
+                contenuHistoire4.text = finBaronSamedi7
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 17
+            }
+            // 4
+        case 4:
+            if sender.tag == 1 {
+                contenuHistoire4.text = optionViolence
+                choix1.setTitle(choix1OptionViolence, for: .normal)
+                choix2.setTitle(choix2OptionViolence, for: .normal)
+                compteurHistoire4 = 16
+            } else {
+                contenuHistoire4.text = optionBoisson
+                choix1.setTitle(choix1OptionBoisson, for: .normal)
+                choix2.setTitle(choix2OptionBoisson, for: .normal)
+                compteurHistoire4 = 5
+            }
+            // 5
+        case 5:
+            if sender.tag == 1 {
+                contenuHistoire4.text = baronApparait
+                choix1.setTitle(choix1BaronApparait, for: .normal)
+                choix2.setTitle(choix2BaronApparait, for: .normal)
+                compteurHistoire4 = 6
+            } else {
+                contenuHistoire4.text = FuiteCimetiere
+                choix1.setTitle(choix1FuiteCimetiere, for: .normal)
+                choix2.setTitle(choix2FuiteCimetiere, for: .normal)
+                compteurHistoire4 = 7
+            }
+            // 6
+        case 6:
+            if sender.tag == 1 {
+                contenuHistoire4.text = finBaronSamedi2
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 9
+            } else {
+                contenuHistoire4.text = FuiteCimetiere
+                choix1.setTitle(choix1FuiteCimetiere, for: .normal)
+                choix2.setTitle(choix2FuiteCimetiere, for: .normal)
+                compteurHistoire4 = 7
+            }
+            // 7
+        case 7:
+            if sender.tag == 1 {
+                contenuHistoire4.text = finBaronSamedi1
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 8
+            } else {
+                contenuHistoire4.text = finBaronSamedi3
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 10
+            }
+            // pas de 8, 9, 10, 12, 14 ou 15
+        case 11:
+            if sender.tag == 1 {
+                contenuHistoire4.text = finBaronSamedi4
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 12
+            } else {
+                contenuHistoire4.text = decisionSeDebarasserDuCorps
+                choix1.setTitle(choix1DecisionSeDebarasserDuCorps, for: .normal)
+                choix2.setTitle(choix2DecisionSeDebarasserDuCorps, for: .normal)
+                compteurHistoire4 = 13
+            }
+            // 13
+        case 13:
+            if sender.tag == 1 {
+                contenuHistoire4.text = finBaronSamedi6
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 15
+            } else {
+                contenuHistoire4.text = finBaronSamedi5
+                choix1.setTitle(rejouerBaronSamedi, for: .normal)
+                choix2.isHidden = true
+                compteurHistoire4 = 14
+            }
+            // 16
+        case 16:
+            if sender.tag == 1 {
+                contenuHistoire4.text = baronApparait
+                choix1.setTitle(choix1BaronApparait, for: .normal)
+                choix2.setTitle(choix2BaronApparait, for: .normal)
+                compteurHistoire4 = 6
+            } else {
+                contenuHistoire4.text = sacrificeRAS
+                choix1.setTitle(choix1SacrificeRAS, for: .normal)
+                choix2.setTitle(choix2SacrificeRAS, for: .normal)
+                compteurHistoire4 = 11
+            }
+            // prise en compte des fins, donne option rejouer soit 8, 9, 10, 12, 14 ou 15
+        default:
+            print("rejouer !")
+            navigationController?.popViewController(animated: true)
+            
+        }
+    }
+    
+    //MARK: On ajoute une option secouer iphone pour revenir aux chapitres
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("motion successful !")
+        navigationController?.popViewController(animated: true)
     }
     
 }
