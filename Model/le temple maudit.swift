1abd3e740f299ed9a622d7e8b251d4975a0628d5
lekysma <jeanmartinkyssama@yahoo.com>
2019-05-18 21:54:45 +0000

    ajoute nouvelle histoire (juste model)

--- Model/le temple maudit.swift
+++ Model/le temple maudit.swift
@@ -7,3 +7,106 @@
 //
 
 import Foundation
+
+// pour rejouer
+let rejouerTempleMaudit : String = "Revenir à l'accueil"
+
+// 1
+let tempete : String = "tempete, on fait quoi ?"
+let choix1Tempete : String = "on etablit le camp sur place" // vers 10
+let choix2Tempete : String = "on va au monastere" // vers 2
+
+// 2
+let monastere : String = "reception monastere, legende"
+let choix1Monastere : String = "ce sont des fictions" // vers 3
+let choix2Monastere : String = "moi je crois" // vers 24
+
+// 3
+let arriveeTemple : String = "arrive dans vallee et decouvre temple"
+let choix1ArriveeTemple : String = "on explore le temple" // vers 4
+let choix2ArriveeTemple : String = "on n'explore pas" // vers 5
+
+// 4
+let ExplorationTemple : String = "visite temple, statues demoniaques, bruit"
+let choix1ExplorationTemple : String = "on enquete le bruit" // vers 11
+let choix2ExplorationTemple : String = "fuyons" // vers 5
+
+// 5
+let attaqueCamp : String = "camp attaqué, je vais voir ce qui se passe ?"
+let choix1AttaqueCamp : String = "je vais enqueter" // vers 12
+let choix2AttaqueCamp : String = "je reste en securité" // vers 6
+
+// 6
+let retourTempleMaudit : String = "retour temple, on rencontre demon qui s'approche"
+let choix1RetourTempleMaudit : String = "je prends mes distances" // vers 7
+let choix2RetourTempleMaudit : String = "je m'approche" // vers 13
+
+// 7
+let possessionProfesseur : String = "professeur possedé, on rentre camp, retour via monastere ou direct vers bangos ?"
+let choix1PossessionProfesseur : String = "on passe par le monastere" // vers 8
+let choix2PossessionProfesseur : String = "rentrons à bangos" // vers 14
+
+// 8
+let retourMonastere : String = "retour monastere, detection du demon, exorcisme necessaire"
+let choix1RetourMonastere : String = "j'assiste a la ceremonie" // vers 9
+let choix2RetourMonastere : String = "je n'assiste pas" // vers 16
+
+// 9
+let finTempleMaudit2 : String = "je participe a l'exorcisme, cela me rend fou. news : je suis interné a saint zonzon a Bangos"
+
+// 10
+let campSurPlace : String = "on perd des hommes. lendemainm visite monastere ou vallee direct ?"
+let choix1CampSurPlace : String = "on visite monastere" //  vers 2
+let choix2CampSurPlace : String = "on part directement" // vers 3
+
+// 11
+let presenceDemon : String = "presence demon detectée, je prends mes distances ou je me rapproche ?"
+let choix1PresenceDemon : String = "je prends mes distances" // vers 7
+let choix2PresenceDemon : String = "je me rapproche" // vers 13
+
+// 12
+let finTempleMaudit1 : String = "je meurs tué; news : pas de nouvelles des explorateurs"
+
+// 13
+let finTempleMaudit4 : String = "je suis possedé, je me dirige vers camp. news : entiere equipe morte, pas de trace assistant"
+
+// 14
+let retourABangos : String = "retour Bangos, meurtres, j'ai des soupçons, je fais quoi ?"
+let choix1RetourABangos : String = "option pere Migoua" // vers 14
+let choix2RetourABangos : String = "option Fifion" // vers 19
+
+// 15
+let visiteMigoua : String = "je vais voir pere Migoua, exorcisme, nous confrontons le demon"
+let choix1VisiteMigoua : String = "je participe a l'exorcisme" // vers 17
+let choix2VisiteMigoua : String = "je m'enfuis" // vers 18
+
+// 16
+let finTempleMaudit3 : String = "succes, mais tous les moines meurent. news : je fais une these sur la spiritualite thibetaine"
+
+// 17
+let finTempleMaudit5 : String = "exorcisme realisé par pretre, je demissione et repars au tibet"
+
+// 18
+let finTempleMaudit6 : String = "durant ma fuite, je suis agressé et je meurs sous les coups. news : pere migoua hospitalisé"
+
+// 19
+let optionFifion : String = "visite Fifion; option : sacrifice corporel ou argent"
+let choix1OptionFifion : String = "je donne une couille" // vers 20
+let choix2OptionFifion : String = "je donne de l'argent" // vers 21
+
+// 20
+let finTempleMaudit7 : String = "rite, demon trop fort, je meurs. news : Fifion a disparu"
+
+// 21
+let riteCimetiere : String = "cimetiere, ceremonie Papa Legba, combat de titans"
+let choix1RiteCimetiere : String = "je reste regarder" // vers 22
+let choix2RiteCimetiere : String = "je pars sans regarder" // vers 23
+
+// 22
+let finTempleMaudit8 : String = "demon tué, vous devenez fou. news : vallee entiere detruite"
+
+// 23
+let finTempleMaudit9 : String = "news : cimetiere entier detruit, grand trou sans fonds apparu"
+
+// 24
+let finTempleMaudit10 : String = "je deserte, rentre Bangos, viré these. news : professeur disparu, reste expedition morte"
