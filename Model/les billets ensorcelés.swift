7326234cb5e6805b5f5218749c8d41a6553f0056
lekysma <jeanmartinkyssama@yahoo.com>
2019-06-10 14:25:21 +0000

    debut écriture histoire billets ensorcelés.

--- Model/les billets ensorcelés.swift
+++ Model/les billets ensorcelés.swift
@@ -8,34 +8,164 @@
 
 import Foundation
 
-let introductionHistoire3 : String = "transporte inconnu, raconte pb, proposition aide inconnu"
-let choix1IntroductionHistoire3 : String = "j'accepte son aide" // vers 2
-let choix2IntroductionHistoire3 : String = "je refuse son aide" // vers 9
+let introductionHistoire3 : String = """
+
+Comme tous les jours, vous avez garé votre taxi moto en plein coeur de la gare routière de Brimborian. Vous et les autres taxis, minibus, bus et autres taxi motos présents arranguez et proposez vos services aux commerçants et autres clients présent à la gare routière.
+
+- 'Taxi moto, taxi moto', vous criez à plein gosier, tentant de vous faire entendre au milieu du bruit assourdissant. Une personne s'approche finalement de vous. Ce qui interpelle en premier, est la mine patibulaire de cet individu : il a le regard lourd, inquisiteur, et semble plus à la recherche d'une personne spécifique. Il vous interpelle :
+
+- 'Je veux allez à Sokotol', commence t-il.
+- 'Cela fera 1000 Bangos', vous répondez. Il tente de discuter le prix à la baisse, mais accepte finalement, de mauvaise grâce, le prix demandé. Bientôt, vous êtes sur la route en direction de Sokotol, une petite bourgade située juste à la sortie de Bangos. Soudain, l'individu vous demande :
+
+- 'Comment tu t'appelles ?'
+- 'Je m'appelle Sonabébé SONAWÉ', vous répondez l'air surpris. Typiquement, vous ne voulez pas de clients trop bavards : vous avez juste envie de faire votre course, et de repartir à la gare routière trouver un nouveau client. Votre réponse brève ne semble pas décourager l'inconnu qui insiste :
+
+- 'Dis mois Sonabébé, est ce que tu est heureux?'. Il fait chaud, la journée a été longue, et vous vous en voulez d'avoir accepté une course lointaine, pour un prix finalement dérisoire...Cela explique peut-être ce que vous dites ensuite:
+- 'Moi je suis pas là pour blablater, je travaille matin et soir pour rien, j'ai peur de tomber malade car j'ai pas d'argent, je me repose jamais, donc laisse moi tranquille, ok! Je suis pas là pour répondre à tes questions.'
+
+Comme vous avez le dos tourné, vous ne distinguez donc pas l'expression sur le visage de votre passager à vos propos. Ce dernier reprend :
+- 'Tu sais qui je suis ?', il vous demande. Excédé, vous l'interrompez :
+- 'Je vous l'ai dit, cela ne m'intéresse pas, j'ai juste envie de de finir ma course et je prie pour trouver un client au retour sinon je conduis a vide'.
+
+Imperturbable, l'étranger continue :
+- 'Mais est ce que tu sais qui je suis ?'
+
+Vous décidez de jouer le jeu :
+- 'Qui es-tu ?'
+
+L'étranger reprend :
+- 'Je m'appelle Moutoukouété RIBANA'. Ce nom vous interpelle, car tout le monde à Bangos a entendu ce nom. Vous l'interpellez, d'un ton plus respectueux :
+- 'Vous êtes de la même famille que ...' vous commencez. Il vous interromp :
+- 'Oui, je suis le neveu de Fifion RIBANA'. Vous ne pouvez vous empêcher de frémir. Fifion RIBANA ou LE RIBANE, comme il est également connu, est tout simplement le sorcier le plus puissant de Bangos. Tout le monde sait qu'il a de grands pouvoirs et que personne n'est assez stupide pour lui manquer de respect. Vous devenez encore plus respectueux.
+
+- 'Je suis désolé si j'ai pu paraitre insolent', c'est juste la journée qui a été longue, vous commencez. Il vous interromp, l'air amusé :
+- 'Ne t'inquiète pas pour ça!, il répond, grand seigneur. 'Honnêtement, j'ai choisi choisi ton taxi moto car j'ai remarqué que tu n'avais pas l'air très heureux. Comme tu le sais, mon oncle est le plus grand sorcier de Bangos. Si tu as des problèmes d'argent, il peut t'aider.'
+
+Le neveu du sorcier vous demande :
+- 'Tu as des economies ?'. Prudemment, vous répondez :
+- 'Oui j'ai quelques economies, mais j'ai pas non plus envie de les perdre hein!'. Soudain impatient, le jeune RIBANA répond :
+- 'Il faut savoir ce que tu veux, si tu veux vraiment de l'aide, tu dois donner quelque chose. On peut utiliser la recette que tu as gagné depuis le debut de la journée. Tu as combien sur toi ?'
+
+Vous fouillez dans votre sac banane :
+- 'J'ai 50 000 bangos', vous lui répondez après quelques secondes. Il vous sourit d'un air encourageant :
+- 'C'est plus que suffisant, si tu apportes ceci à mon oncle, il pourra le multiplier. Tu ressortiras avec 5 millions
+
+Vous réflechissez à sa proposition.
+
+"""
+let choix1IntroductionHistoire3 : String = "Après tout pourquoi pas ?" // vers 2
+let choix2IntroductionHistoire3 : String = "Je ne suis pas intéressé" // vers 9
 
 // compteur = 2
-let propositionSacrifice : String = "conduis a fifion, explique sacrifice, tu veux gagner de l'argent ?"
-let choix1PropositionSacrifice : String = "je suis d'accord" // vers 3
-let choix2PropositionSacrifice : String = "non merci" // vers vers 9
+let propositionSacrifice : String = """
+Vous faites demi tour et revenez vers Bangos. Vous suivez les instructions du jeune homme et vous arrêtez derrière le cimetière de Brimborian. Une vieille case, branlante et brimbalante se dresse, parmi les pierres tombales, au milieu des herbes.
+
+- 'Arrête toi ici et suis moi.', le jeune homme vous ordonne. Vous vous frayez un chemin au milieu des herbes hautes avant d'arriver devant la case. Le neveu du sorcier frappe à la porte :
+- 'Oncle Fifion! Oncle Fifion, j'ai ramené quelqu'un pour toi!.
+
+Après quelques secondes, la porte s'ouvre et laisse filtrer quelques rayons solaires en cette fin d'après midi. La personne qui vient d'ouvrir la case n'est pas exactement impressionnante : il s'agit d'un vieil homme chauve, ridé, portant un vieux pagne déchiré. On remarque ses ongles, longs et jaunes. Son regard par contre sombre, agressif voire maléfique est la preuve que vous vous tenez en face d'un homme redoutable. Il sourit, revelant une machoire édentée :
+
+- 'Moutoukouété! Tu fais quoi ici, entre, entre!'.
+
+Vous suivez votre nouveau bienfaiteur dans la case : celle - ci est aussi délabrée que son aspect extérieur semblait le suggérer. Une natte et un foyer avec un feu allumé sont la seule preuve qu'elle est habitée.
+
+- 'Je te présent mon ami Sonabébé SONAWÉ', le neveu du sorcier commence. 'Il est taxi moto, et comme il a des problèmes d'argent, il a ramené sa prise de la journée. je lui ai dit que tu pourrais l'aider.'
+
+Le vieil homme vous regarde d'un air perçant :
+- 'Dis moi, est ce que tu es prêts à tout faire pour gagner de l'argent ?'
+- 'Bien sur sorcier', vous lui répondez. 'Je suis fatigué d'être pauvre. Je vous ai ramené ma prise de la journée.', vous lui tendez l'argent.
+
+En prenant la liasse de billets froissés, le sorcier vous dit :
+- 'Moi je suis sorcier, tu me donnes 50 000 bangos, je peux pas multiplier ça comme cela. Il s'agit de faire un sacrifice.'. Il poursuit : 'Si tu es d'accord, moi je vais faire invocations là, et faire multiplication, mais faut que quelqu'un meure. Tu dois choisir une personne : cette personne va mourir. Une fois qu'elle est morte, l'argent va apparaitre dans ta maison. Tu dis quoi ?'
+
+Quelle est votre décision ?
+
+"""
+let choix1PropositionSacrifice : String = "Cela me va" // vers 3
+let choix2PropositionSacrifice : String = "Non! Pas pour de l'argent" // vers vers 9
 
 // compteur = 3
-let sacrificeChien : String = "amene chien, sur le point de l'egorger"
-let choix1SacrificeChien : String = "je laisse faire" // vers 4
-let choix2sacrificeChien : String = "j'interviens" // vers 10
+let sacrificeChien : String = """
+
+Le sorcier se tourne vers son neveu.
+
+- 'J'élève des chiots là, va derrière la case et rapporte moi un chiot', il lui ordonne.
+- 'Bien tonton', ce dernier répond. Quelques minutes plus tard, il revient dans la case. Il tient dans ses bras un jeune chiot qui lui lèche nerveusement la main.
+- 'Amènes-le près du feu! Attend je vais chercher le couteau.', le sorcier indique le foyer.
+
+Vous les interrompez, horrifié.
+
+- 'Surement vous n'allez pas sacrifier ce chien!'
+
+Le sorcier vous regarde amusé.
+
+- 'Je m'excuse, c'est vrai que je t'ai pas expliqué. Je vais trancher la gorge de ce chien, et pendant qu'il agonise, je vais verser le sang sur quelques billets. Ce sont ces billets là qui vont servir au sacrifice.'
+"""
+let choix1SacrificeChien : String = "OK, merci d'avoir expliqué" // vers 4
+let choix2sacrificeChien : String = "Je ne vais pas laisser faire ça" // vers 10
 
 // compteur 4
-let billetsCarrefour : String = "chien egorgé, arrive carrefour, pose billets"
-let choix1BilletsCarrefour : String = "Je reste regarder" // vers 5
-let choix2BilletsCarrefour : String = "Je rentre chez moi" // 11
+let billetsCarrefour : String = """
+
+Le sang artériel qui coule de la gorge du chiot parait presque noir dans la lumière du crépuscule. Le sorcier, l'air d'avoir effectué quelque chose de banal, se tourne vers vous et vous dit :
+
+- 'Tu vous les billets là. Dès que la nuit tombe tu vas dehors. Tu vas dans un endroit où il y a du monde et tu poses les billets. Après tu te caches et tu attends. Si quelqu'un ramasse les billets, elle va mourir. Tu rentres alors chez toi. Demain matin, tu trouveras beaucoup d'argent dans ta maison.'
+
+Le sorcier vous regarde soudain d'un air très sérieux :
+- 'Une fois que tu as donné les billets, tu les reprends pas!' Il vous dit. 'Si tu reprends les billets, c'est toi qui va mourir!. Donc si tu vois que quelqu'un a pris les billets, tu t'en vas. Sinon, c'est toi-même toi-même!'.
+- 'J'ai compris sorcier', vous lui répondez gravement. 'Je vais aller au carrefour Motabo, il y a beaucoup de restaurants là bas, des gends qui vendent de la viande grillée. Je vais poser les billets au milieu de la rue, et normalement il devrait y avoir quelqu'un.'
+- 'C;est ton problème!', le vieil homme vous interromp. 'Moi j'ai fait la magie, le reste c'est toi-même qui fait'.
+
+ENVIRON UNE HEURE PLUS TARD
+La nuit recouvre à présent Bangos de son manteau sombre. Vous êtes caché dans des buissons à proximité, en train d'observer un vendeur de grillades entourés de clients affamés. 5 minutes plus tôt, vous avez laissé tomber les billets dans un coin éclairé. normalement, ils devraient être remarqués très vite.
+
+Vous vous demandez s'il est nécessaire de rester regarder ou si vous pouvez rentrer dès à présent chez vous.
+"""
+let choix1BilletsCarrefour : String = "J'observe ce qui se passe" // vers 5
+let choix2BilletsCarrefour : String = "Je peux rentrer chez moi" // 11
 
 // compteur = 5
-let accident : String = "accident, je me rapproche, on me tend quelque chose"
-let choix1Accident : String = "je prends sans regarder" // vers 6
-let choix2Accident : String = "je refuse" // vers 12
+let accident : String = """
+
+Vous décidez de suivre les recommandations du sorcier et resté caché en attendant que quelqu'un ramasse les billets.
+
+Un des clients, se baisse soudain et lève triomphalement les bras. On distingue vaguement les billets dans sa mains. Sous les félicitations des autres clients, il s'empresse de payer sa commande. Bientôt, un paquet de grillades en mains, il s'apprête à traverser la route.
+
+Un bus surgit de nulle part et lui roule dessus! Vous êtes aussi choqué que tous les autres : personne n'a rien vu venir. Tout le monde converge vers le lieu de l'accident. Plusieurs personnes accourent pour porter secours au blessé. D'autres appellent une ambulance. Vous décidez de profiter du tumulte ambiant pour partir.
+
+Vous quittez votre cachette au moment même où la première ambulance arrive. Vous croisez le brancard qui transporte le defunt vers l'ambulance. Un des secouristes vous tend quelque chose.
+
+- 'Tenez monsieur, cela pourrait vous servir', le secouriste vous dit.
+"""
+let choix1Accident : String = "Je prends sans regarder" // vers 6
+let choix2Accident : String = "Je refuse" // vers 12
 
 // compteur = 6
-let conseilFifion : String = "reviens voir fifion, demande aide, conseil : dibambari"
-let choix1ConseilFifion : String = "j'accepte d'y aller" // vers 7
-let choix2ConseilFifion : String = "c'est trop dangereux" // vers 13
+let conseilFifion : String = """
+
+Vous acceptez ce qu'il vous tend sans regarder et continuez votre chemin. Lorsque vous pensez á regarder de quoi il s'agit, vous réalisez avec horreur qu'il s'agit des billets envoutés, des billets que vous aviez caché il y a quelques minutes. Des billets qui se retrouvent à présent en votre possession!
+
+Encore sous le choc, vous vous dépêchez de repartir au domicile du sorcier Fifion.
+
+- 'Sorcier Fifion! Sorcier Fifion, ouvre-moi!'. Vous êtes en larmes, implorant ce dernier de vous ouvrir.
+
+Il ouvre la porte et vous regarde d'un air mauvais :
+- 'Tu fais quoi ici, tu veux quoi ?', il vous demande.
+- 'Sorcier, il faut que tu m'aides!'. les mots ont du mal à sortir tellement vous êtes nerveux :
+
+- 'J'ai fait comme tu as dit, j'ai déposé l'argent, et y a quelqu'un qui a ramassé... Et il est mort. Et moi je passe pour moi ta ta ta, et y a quelqu'un qui me donne quelque chose sorcier.' Vous continuez, en pleurs.
+- 'C'étaient les billets sorcier, j'ai pas fait attention, j'ai pris comme ça, mais c'étaient les billets sorcier. J'ai pas envie de mourir sorcier. Aide moi!'
+
+Fifion vous regarde avec mépris :
+- 'Tu es un idiot, on te dit n'accepte rien, et tu te débrouilles pour reprendre les billets, tu crois que je blague ? Moi je t'ai dit que si quelqu'un prend les billets il meurt, il est mort non ?', il vous demande.
+- 'Tu as raison sorcier', vous répondez, la morve au nez. 'Je suis désolé sorcier, aide moi, pardon!'.
+- 'Ecoutes-moi bien idiot! Si tu fais pas ce que je dis, tu vas mourir : tu prends les billets là, tu cours tu cours jusqu'au quartier de Dibambari, le quartier où il y a des voyous. Il y a toujours des gens là bas qui cherchent l'argent. Tu approches un bandit, et tu lui donnes l'argent comme ça. Tu te débrouilles, sinon, c'est ton problème.' Il reprend :
+- 'Moi, je te connais pas maintenant sors de chez moi!'. Avec une force surprenante pour quelqu'un d'aussi agé, il vous pousse hors de sa case.
+
+Tout seul dans le noir, vous repensez au conseil de Fifion, qu'allez-vous faire ?
+"""
+let choix1ConseilFifion : String = "J'y vais, je n'ai pas le choix" // vers 7
+let choix2ConseilFifion : String = "C'est trop dangereux" // vers 13
 
 // compteur = 7
 let propositionBillets : String = "j'y arrive, propose billets"
