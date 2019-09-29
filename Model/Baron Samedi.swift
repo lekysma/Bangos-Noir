3431894f93e09ebc07c1cf03dcfb57e9b5ad5acd
lekysma <jeanmartinkyssama@yahoo.com>
2019-09-29 21:11:53 +0000

    details histoire Baron samedi. moitié faite

--- Model/Baron Samedi.swift
+++ Model/Baron Samedi.swift
@@ -12,37 +12,175 @@ import Foundation
 let rejouerBaronSamedi = "Rejouer"
 
 // 1
-let vaChercherGare : String = "Je vais chercher Fifion a la gare"
-let choix1VaChercherGare : String = "je consulte la photo donnée" // vers 3
-let choix2VaChercherGare : String = "je ne consulte pas la photo" // vers 2
+let vaChercherGare : String = """
+Il fait chaud en ce dimanche après-midi à la gare de Fort Canal, une cité balnéaire située à 50 km au Nord De Bangos. Vous avez conduit votre patron et employeur Timothée Eustache MAKANDAL. Ce dernier vous a chargé d'aller attendre un invité, qui doit arriver dans le prochain train en provenance de Bangos. C'est la raison pour laquelle vous vous tenez sur le quai.
+
+20 minutes plus tard, la vieille locomotive à vapeur qui effectue péniblement la liaison avec Bangos arrive enfin. Les voyageurs se hâtent de descendre, comme pressés de vaquer à leurs occupations respectives. La personne que vous attendez s'appelle Fifion RIBANAT. Votre patron vous a donné une photo. Deux personnes, parmi les dernières à descendre semblent correspondre.
+"""
+let choix1VaChercherGare : String = "Je consulte de nouveau la photo" // vers 3
+let choix2VaChercherGare : String = "Je n'ai pas besoin de rafraîchir ma mémoire" // vers 2
 // 2
-let mauvaixChoix : String = "mauvaise personne, qu'est ce que je fais"
-let choix1MauvaisChoix : String = "je regarde la photo" // vers 3
-let choix2MauvaisChoix : String = "je rentre bredouille" // vers 3 avec une variation
+let mauvaixChoix : String = """
+Vous vous dirigez vers un homme d'une soixantaine d'années. Ce dernier porte un vieux costume, ainsi qu'un casque colonial jaunit par le temps et le soleil. On dirait un instituteur à la retraite.
+
+Vous vous approchez de lui d'un air respectueux (votre patron a insisté sur le fait de ne pas froisser votre invité) :
+
+- 'Fifion RIBANA ?', vous commencez. le vieil homme sursaute et vous regarde d'un air effrayé.
+- 'Moi ?!! Je ne suis pas Fifion, laissez-moi tranquille ! Je ne suis pas cette personne ! Il apperçoit un couple plus jeune à distance et se dépêche de le rejoindre, l'air soulagé.
+
+Vous le regardez partir d'un air dépité et vous demandez quoi faire.
+"""
+let choix1MauvaisChoix : String = "Je regarde la photo" // vers 3
+let choix2MauvaisChoix : String = "Je rentre bredouille" // vers 3 avec une variation
 // PS : pas de choix 2
 //3
-var necessiteFaireSacrifice : String = "je conseille mon patron : necessaire de le faire ?"
-let choix1NecessiteFaireSacrifice : String = "vous avez raison patron" // vers 4
-let choix2NecessiteFaireSacrifice : String = "vous avez tort patron" // vers 17
+var necessiteFaireSacrifice : String = """
+Quelques minutes plus tard vous vous dirigez vers la résidence de votre patron. C'est une imposante villa située à la Marina Del Rey, au bord d'une plage privée réservée exclusivement à l'usage des personnes les plus fortunées de la région.
+
+A l'arrière de la limousine, votre patron et la personne que vous êtes venu chercher discutent à voix basse.
+
+Ce dernier est une surprise. Il s'agit d'un vieil homme chauve et ridée vêtu d'un pagne sale, plusieurs fois raccommodé. Des sourcils gris, touffus n'arrivent pas à cacher une expression qui surprend par sa férocité. Un air général de malveillance se dégage de cet étrange personnage.
+
+Vous ne pouvez vous empêcher de vous demandez ce qu'un homme aussi prospère que MAKANDAL peut avoir en commun avec lui.
+
+Arrivé à la villa, vous transportez le baluchon du vieil homme vers l'une des innombrables chambres d'amis de la résidence. Vous vous dirigez vers le fumoir où votre patron s'est dirigé avec son invité.
+
+- 'Apporte-nous à boire !', votre patron vous ordonne. 'Vous prendrez bien un bourbon, n'est ce pas Fifion?', celui-ci demande au vieil homme.
+- 'Je veux bien une petite rasade', ce dernier opine de la tête. Vous versez un doigt de bourbon dans le verre que vous lui tendez. Le vieil homme vous interpelle impoliment :
+- 'C'est quoi cette quantité ? Donne plus !, il vous ordonne. Votre patron vous confirme l'air impérieux :
+- 'Allons, sers-lui plus de bourbon, le trajet en train a été long. Et nous ne manquons pas exactement de bourbon....'
+
+Pendant que les deux trinquent, vous vous mettez poliment à l'écart. Le sorcier vide son verre d'un coup et demande brusquement :
+- 'Tu sais qui je suis ? Je suis Fifion RIBANA, le plus grand sorcier de Bangos. Tu m'as appelé chez toi pour quoi ?
+
+Votre patron pose également son verre et commence prudemment :
+- 'Je sais que vous êtes le plus grand sorcier de Bangos. C'est la raison pour laquelle je vous ai invité. Vous voyez, je suis l'homme le plus riche de Fort Canal, je possède la Marina, et le plus grand hotel de la ville. Mais je veux plus, je veux être élu Maire de la ville. Le problème, c'est que les gens ne m'aiment pas; ils sont probablement jaloux de ma réussite, mais ils ne voteraient jamais pour moi. J'ai donc besoin des services d'un spécialiste en nécromancie.
+
+Fifion le regarde d'un air franc, à la limite de l'insolence.
+- 'Tout d'abord, faut me payer, je travaille pas cadeau.'; sans un mot, votre patron sort des liasses de bangos d'une serviette posée sur la table et les tend vers le sorcier.
+
+- 'C'est un bon début.', ce dernier continue. Je vais vous conseiller, mais c'est vous qui allez tout faire. Moi je vais rien toucher'
+- 'Compris sorcier', votre patron indique. 'Que dois-je faire ?'
+
+Le sorcier continue :
+- 'Vous devez trouver un sacrife, l'amener au cimetière, et lui arracher le coeur sur une tombe. Vous garderez ce coeur dans une urne. L'esprit du mort sera piégé et va travailler pour vous.'
+
+MAKANDAL se tourne soudain vers vous. Vous sursautez lorsqu'il vous adresse la parole :
+- 'Dis-moi, nous n'avons pas un pêcheur emprisonné au sous-sol ? Tu sais celui que tu avais surpris en train de pêcher sur notre plage privée.'
+- 'Ah oui ! Il s'appelle Estimé JOLI-COEUR. Il est dans un de nos cachots', vous confirmez.
+
+- 'Bien !', le sorcier hoche la tête. 'Nous ferons le sacrifice cette nuit. Je vais me préparer.
+
+Tout seul avec votre patron, vous osez exprimer vos craintes.
+- 'Je ne pense pas qu'on devrait faire ça patron. Un sacrifice humain ? Et en plus dans un lieu public ...', vous commencez. Ce dernier vous interromp :
+
+- 'Tais-toi ! Tu ne sais pas ce que cela signifie pour moi et ma carrière. Je n'ai pas envie de rester à Fort Canal. J'ai envie de rejoindre Bangos, le gouvernement, devenir ministre, peut-être renverser ce dictateur de Mangani Mangoua. Le chemin commence par ce petit sacrifice.
+
+Vous vous demandez quoi faire.
+"""
+let choix1NecessiteFaireSacrifice : String = "Je suis avec vous patron" // vers 4
+let choix2NecessiteFaireSacrifice : String = "Vous avez tort, je refuse de vous suivre" // vers 17
 //4
-let chercherPrisonnier : String = "je vais chercher prisonnier, comment le preparer pour transport vers cimetiere ?"
-let choix1ChercherPrisonnier : String = "je le frappe" // vers 16
-let choix2ChercherPrisonnier : String = "je le saoule" // vers 5
+let chercherPrisonnier : String = """
+La nuit est déja avancée lorsque vous vous dirigez vers les cachots situés au sous-sol de la résidence de MAKANDAL. Vous en ressortez en tirant un homme malgré les efforts - impuissants, il semble ne pas avoir mangé depuis plusieurs jours - de ce dernier.
+
+Vous vous demandez comment faire pour le transport vers la limousine qui attend dans la cour.
+"""
+let choix1ChercherPrisonnier : String = "Je le frappe" // vers 16
+let choix2ChercherPrisonnier : String = "Je le saoule" // vers 5
 //5
-let optionBoisson : String = "le prisonnier vomit sur moi, on arrive cimmetiere, Fifion conseille offrande"
-let choix1OptionBoisson : String = "on n'a ni le temps ni le materiel" // vers 6
-let choix2OptionBoisson : String = "peut-être qu'on devrait ecouter l'expert" // vers 7
+let optionBoisson : String = """
+Vous forcez une bouteille entre ses lèvres.
+-'Allez bois !', vous ordonnez. Malgré ses protestations, vous le forcez à avaler plusieurs gorgées de rhum. Son estomac vide - et sans doute une longe familiarité avec l'alcool - signifie qu'il termine très vite la bouteille.
+
+Incohérent, il titube dans votre direction, et finit par vomir sur vos chaussures. Une puanteur atroce se répand. Les lèvres pincées, vous le forcez à entrer dans le coffre de la limousine. Votre patron et le sorcier - son baluchon sur les genoux - sont déjà sur la banquette arrière.
+
+- 'Tout est bon?', votre patron demande l'air impatient. Vous confirmez.
+- 'Alors allons-y ! Il se fait tard.' Votre patron continue.
+
+Une vingtaine de minutes plus tard, vous vous arrêtez devant le portail du cimetière municipal de Fort Canal de St Ixion. Des nuages sombres obscurcissent un ciel, donnant l'impression que la lune elle-même ne souhaite pas être témoin de ce qui est sur le point de prendre place. Un hibou solitaire perce le silence nocturne d'un hululement sinistre.
+
+Vous êtes pris malgré vous par cette atmosphère angoissante. Votre patron ne semble pas touché et vous ordonne sèchement d'aller chercher le sacrifice. A votre retour, Fifion s'adresse soudain à votre patron :
+- 'Nous sommes sur le point de commencer le sacrifice. Avant d'entrer dans le cimetière, je recommande qu'on laisse une offrande à Baron Samedi.'
+
+MAKANDAL le regarde impatiemment :
+- 'Qui est ce Baron Samedi ?' Le sorcier poursuit, l'air sérieux :
+- 'Cest un esprit, c'est le gardien des cimetières. Pour l'apaiser, laissez une bouteille de rhum et quelques cigares. Laissez-les à l'entrée du cimetière et après on peut continuer.'
+
+Votre patron prend très mal ce nouveau contretemps :
+- 'Fifion, je suis déjà venu dans ce cimetière, même en soirée. Je n'ai jamais laissé quoi que ce soit. Et mes collègues qui font des rites ici n'ont jamais mentionné des cadeaux à un Baron machin quelque chose !'
+
+Vous intervenez respectueusement.
+
+"""
+let choix1OptionBoisson : String = "Nous n'avons ni le temps, ni les offrandes" // vers 6
+let choix2OptionBoisson : String = "Peut-être qu'on devrait écouter l'expert" // vers 7
 //6
-let baronApparait : String = "le patron sacrifie prisonnier, le baron apparait"
-let choix1BaronApparait : String = "on resiste" // vers 9
+let baronApparait : String = """
+- 'Nous n'avons le temps !', tranche MAKANDAL. 'Dépêchons-nous, il est déjà tard et je veux en finir.', il termine.
+
+Le sorcier le regarde le visage fermé :
+- 'Ce que vous faites, c'est vous-même, vous même ! Moi j'ai dit ce que je devais dire !'
+- 'Allons sorcier, ne t'inquiète pas tout vas bien se passer.', MAKANDAL insiste d'un air encourageant.
+
+C'est avec beaucoup de réticence que le sorcier vous dirige vers l'une des tombes les plus vieilles du cimetière. Vous déposez votre fardeau sur la pierre tombale. Celui-ci proteste faiblement :
+- 'On est où, vous allez faire quoi avec moin ?'
+- 'On va t'arracher le coeur !', intervient MAKANDAL, l'air féroce. 'On va te sacrifier sur cette tombe. Maintenant laisse-toi faire.
+
+Le prisonnier se met à sangloter :
+- 'Pas arracher coeur à moin, fais pas ça ! Pas coeur à moin !'
+
+Vous arrivez sans problèmes à l'empêcher de bouger. Dans son état, il n'offre qu'une faible résistance. Bientôt, MAKANDAL se tient devant lui et plonge le poignard de toute ses forces dans la poitrine du prisonnier. La mort est presque immédiate. MAKANDAL se sert du couteau de sacrifice pour ouvrir bien grand le sternum de la victime, et bientôt, il tient entre ses mains une petite boule encore palpitante.
+
+Soudain, un grand bruit se fait entendre, vous sentez la pierre tombale sur laquelle vous êtes, glisser, s'ouvrir. Vous perdez l'équilibre et tombez au sol, suivi de votre patron. Il semble que tous les animaux des environs se sont réveillés : un concert d'aboiements, de hululements, de cris divers se fait entendre.
+
+Une silhouette émerge de la tombe : in s'agit d'un homme grand, au teint d'un noir de jais. Il est vêtu d'un chapeau haut de forme et d'une redingote noire. Un air absolu d'autorité se dégage de lui. Il n'y a pas de doute dans votre esprit qu'il s'agit bien de Baron Samedi, celui dont Fifion vous parlé.
+
+- 'Que faites-vous ici ? Pourquoi venez-vous profaner les morts ? Où sont mes cigares, où est mon rhum ?', le Baron vous demande.
+- 'Sorcier, on fait quoi ?', MAKANDAL demande. Vous aussi cherchez le sorcier du regard. Il semble s'être eclipsé sans vous prévenir.
+
+Que décidez-vous de faire ?
+"""
+let choix1BaronApparait : String = "On résiste" // vers 9
 let choix2BaronApparait : String = "Fuyons" // vers 7
 //7
-let FuiteCimetiere : String = "nous fuyons, retour a la residence, decision"
-let choix1FuiteCimetiere : String = "je monte la garde" // vers 8
-let choix2FuiteCimetiere : String = "je m'enfuis" // vers 10
+let FuiteCimetiere : String = """
+Sans vous consulter, votre patron et vous vous dirigez à toute vitesse vers la sortie du cimetière. Vous vous frayez un chemin en trébuchant aux milieu des tombes. Plusieurs fois, vous êtes tenté de regarder en arrière, mais vous savez que quelque chose de néfaste vous arriverait.
+
+- 'Attends-moi !', MAKANDAL vous demande. Peu à peu, vous avez pris de la distance par rapport à lui. Sourd à ses appels, vous continuez à courir. Après une éternité, l'entrée du cimetière apparait enfin. Vous vous dépechez de déverouiller les portières de la limousine. Vous devez vous y prendre plusieurs fois, car vos mains tremblent.
+
+Vous jetez un coup d'oeil rapide vers le cimetière. Votre patron court, une expression de peur abjecte sur le visage. Derrière lui, se profile la silhouette du Baron. A bout de souffle, MAKANDAL parvient enfin à entrer dans la voiture (vous aviez laissé la porte du siège passager ouverte.
+
+- 'Démarre ! Démarre, voyons !', il vous ordonne. Sans dire un mot, vous obtempérez. Vous roulez à toute allure vers la résidence de votre patron. Celui-ci ne dit pas un mot, mais vous lance de temps en temps un regard noir. Vous vous doutez qu'il n'a pas apprecié que vous ne l'ayez pas attendu.
+
+En très peu de temps, vous arrivez enfin au domicile de MAKANDAL. Dès que la voiture s'arrête, il sort et se précipite vers la villa. Il vous lance :
+- 'Monte la garde, et assure toi que personne n'entre. Prends le fusil qui se trouve dans le salon.'; il continue sans vous regarder.
+
+Vous vous demandez ce que vous devez faire.
+"""
+let choix1FuiteCimetiere : String = "Je décide de monter la garde" // vers 8
+let choix2FuiteCimetiere : String = "C'est trop dangereux, je pars d'ici" // vers 10
 //8
-let finBaronSamedi1 : String = "j'affronte baron, meurs. news : crime horrible chez Makandal"
-// on rejouer, pas de choix 2
+let finBaronSamedi1 : String = """
+- 'Oui patron', vous marmonez. Personne ne vous entends. La maison est bien sombre et vous parait trop isolée. La présence du fusil que vous utilisez parfois pour chasser les individus qui pénètrent sur le terrain de votre patron vous parait bien dérisoire. Que peut une telle arme face à un esprit ?
+
+Soudain, un concert d'aboiements troue le silence pesant. Il semble que ce soient tous les chiens du quartier qui hurlent à la mort. C'est presque d'un air résigné que vous voyez la sihouette de Baron Samedi apparaitre.
+
+Vous tirez, une fois, deux fois; vous êtes certain d'avoir vu les balles transpercer le corps du Baron, mais ce dernier continue d'avancer vers vous sans se presser. Ce n'est plus que quelques mètres qui vous séparent.
+
+Une douleur atroce vous plie soudain en deux : sans le réaliser, le Baron vous a transpercé la poitrine avec son bras. Vous sentez ses doigts s'affairer dans votre cage thoracique, et ressortir de votre poitrail avec un bruit dégoutant de suction.
+
+Il brandit sa trouvaille devant vous : votre coeur, palpitant entre ses doigts. Vous semblez le regardez de très loin à présent : vous vous êtes écroulé sans le réaliser. Alors que vos yeux se ferment, que votre corps se vide de son sang, une dernière pensée vous vient :
+- 'J'aurais dû écouter mon oncle et devenir chasseur au village...'
+
+.... EXTRAIT DU BANGOS TIME ....
+CRIME ODIEUX CHEZ MAKANDAL
+
+Aux premières heures du matin, les corps de Timothée Eustache MAKANDAL et de son chauffeur ont été retrouvés par la femme de ménage. La police ne donne pas de détails, mais d'après nos sources, il semble qu'il s'agirait d'un double meurtre rituel...
+
+.... FIN ....
+"""
+// on rejoue, pas de choix 2
 // 9
 let finBaronSamedi2 : String = "j'affronte baron samedi, meurs, news : crime bizarre cimetiere"
 // on rejoue, pas de choix 2
@@ -67,9 +205,30 @@ let finBaronSamedi5 : String = "RAS, je finis par demissionner; news : decouvert
 let finBaronSamedi6 : String = "controle de police, le policier decouvre le corps. on se dechire au proces"
 // on rejoue, pas de choix 2
 //16
-let optionViolence : String = "prisonnier ensanglanté, on arrive au cimetiere, Fifion conseille offrande"
-let choix1OptionViolence : String = "on n'a ni le temps, ni le matos" // vers 6
-let choix2OptionViolence : String = "peut-etre qu'on devrait ecouter l'expert" // vers 11
+let optionViolence : String = """
+prisonnier ensanglanté, on arrive au cimetiere, Fifion conseille offrande
+
+Vous prenez le temps de passer à tabac le prisonnier afin de vous assurer de sa coopération. C'est une loque ensanglantée que vous forcez sans résistance dans le coffre de la limousine. Vous pouvez prendre le volant de la limousine où vous attendent déjà votre patron et le vieux sorcier.
+
+- 'Tout est bon?', MAKANDAL demande l'air impatient. Vous confirmez.
+- 'Alors allons-y ! Il se fait tard.', il continue.
+
+Une vingtaine de minutes plus tard, vous vous arrêtez devant le portail du cimetière municipal de Fort Canal de St Ixion. Des nuages sombres obscurcissent un ciel, donnant l'impression que la lune elle-même ne souhaite pas être témoin de ce qui est sur le point de prendre place. Un hibou solitaire perce le silence nocturne d'un hululement sinistre.
+
+Vous êtes pris malgré vous par cette atmosphère angoissante. Votre patron ne semble pas touché et vous ordonne sèchement d'aller chercher le sacrifice. A votre retour, Fifion s'adresse soudain à votre patron :
+- 'Nous sommes sur le point de commencer le sacrifice. Avant d'entrer dans le cimetière, je recommande qu'on laisse une offrande à Baron Samedi.'
+
+MAKANDAL le regarde impatiemment :
+- 'Qui est ce Baron Samedi ?' Le sorcier poursuit, l'air sérieux :
+- 'Cest un esprit, c'est le gardien des cimetières. Pour l'apaiser, laissez une bouteille de rhum et quelques cigares. Laissez-les à l'entrée du cimetière et après on peut continuer.'
+
+Votre patron prend très mal ce nouveau contretemps :
+- 'Fifion, je suis déjà venu dans ce cimetière, même en soirée. Je n'ai jamais laissé quoi que ce soit. Et mes collègues qui font des rites ici n'ont jamais mentionné des cadeaux à un Baron machin quelque chose !'
+
+Vous intervenez respectueusement.
+"""
+let choix1OptionViolence : String = "On n'a ni le temps, ni les offrandes" // vers 6
+let choix2OptionViolence : String = "Peut-être que nous devrions écouter le sorcier" // vers 11
 // 17
 let finBaronSamedi7 : String = "je demissionne, news : makandal a gagné elections, il meurt par la suite"
 
