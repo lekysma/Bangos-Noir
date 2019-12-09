ae493ddf8dc9480783fadf96c4ffeb8a203bb824
lekysma <jeanmartinkyssama@yahoo.com>
2019-12-09 17:01:59 +0000

    resolution du problème de duplication des tableview cell chaque fois qu'on se déconnecte et on se déconnecte. mis l'ensemble des données dans le view controller gérant le table view. voir s'il y a pas une méthode plus élégante de separation des données.

--- Model/modele donnees table view cell.swift
+++ Model/modele donnees table view cell.swift
@@ -20,28 +20,13 @@ class ContenuCellule {
 }
 
 
-//MARK: On cree un tableau qui contient les noms de toutes les images
-var tableauImages = ["mbigiMuntu1", "taureauDeboeuf1", "billets1", "baronSamedi1", "temple" ]
-//MARK: On cree ensuite un tableau qui va renfermez la structure de chaque cellule de notre application
-var tableauDonnees = [ContenuCellule]()
+
 
 //MARK: Enfin on cree une fonction qui va detailler le contenu de chaque cellule de l'application
 
-func contenuDeChaqueCellule () {
-    // on cree des objets sur la base de la classe plus haut
-    let chapitre1 = ContenuCellule(label: "Le Mbigi Muntu", image: tableauImages[0])
-    let chapitre2 = ContenuCellule(label: "Taureau de boeuf", image: tableauImages[1])
-    let chapitre3 = ContenuCellule(label: "Les billets ensorcelés", image: tableauImages[2])
-    let chapitre4 = ContenuCellule(label: "Baron Samedi", image: tableauImages[3])
-    let chapitre5 = ContenuCellule(label: "Le temple maudit", image: tableauImages[4])
-    
-    // on ajoute ces objets au tableau tableauDonnees
-    tableauDonnees.append(chapitre1)
-    tableauDonnees.append(chapitre2)
-    tableauDonnees.append(chapitre3)
-    tableauDonnees.append(chapitre4)
-    tableauDonnees.append(chapitre5)
-}
+
+
+
 
 
 
--- View Controller/ChapitresTableViewController.swift
+++ View Controller/ChapitresTableViewController.swift
@@ -12,17 +12,23 @@ import Firebase
 // on importe SVProgressHUD
 import SVProgressHUD
 
-let tableauSegues = ["histoire1", "histoire2", "histoire3", "histoire4", "histoire5"]
-
 class ChapitresTableViewController: UITableViewController {
+    
+    let tableauSegues = ["histoire1", "histoire2", "histoire3", "histoire4", "histoire5"]
+
+    //On cree un tableau qui va renfermez la structure de chaque cellule de notre application
+    var tableauDonnees = [ContenuCellule]()
+
+    //MARK: On cree un tableau qui contient les noms de toutes les images
+    var tableauImages = ["mbigiMuntu1", "taureauDeboeuf1", "billets1", "baronSamedi1", "temple" ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        
         contenuDeChaqueCellule()
-        
+        print("table view cell displayed")
         // on affiche uniquement les cellules pleines
         tableView.tableFooterView = UIView()
     }
@@ -71,9 +77,10 @@ class ChapitresTableViewController: UITableViewController {
             //si la deconnexion reussit, on fait disparaitre SVProgressHUD
             SVProgressHUD.dismiss()
             
+            tableView.reloadData()
             performSegue(withIdentifier: "VersAccueil", sender: self)
             
-            print("j'ai bien été redirigé vers l'accueil et je suis deconnecté avec succcès")
+            print("je suis deconnecté avec succcès")
         }
         catch {
             
@@ -83,6 +90,26 @@ class ChapitresTableViewController: UITableViewController {
         
         //Auth.auth().signOut()
     }
+    
+    //MARK: - On liste ici la fonction qui contient les differentes categories
+    func contenuDeChaqueCellule () {
+        
+        // on cree ensuite des objets sur la base de la classe plus haut
+        let chapitre1 = ContenuCellule(label: "Le Mbigi Muntu", image: tableauImages[0])
+        let chapitre2 = ContenuCellule(label: "Taureau de boeuf", image: tableauImages[1])
+        let chapitre3 = ContenuCellule(label: "Les billets ensorcelés", image: tableauImages[2])
+        let chapitre4 = ContenuCellule(label: "Baron Samedi", image: tableauImages[3])
+        let chapitre5 = ContenuCellule(label: "Le temple maudit", image: tableauImages[4])
+        
+        // on ajoute ces objets au tableau tableauDonnees
+        tableauDonnees.append(chapitre1)
+        tableauDonnees.append(chapitre2)
+        tableauDonnees.append(chapitre3)
+        tableauDonnees.append(chapitre4)
+        tableauDonnees.append(chapitre5)
+    }
+
+    
     
     
     
